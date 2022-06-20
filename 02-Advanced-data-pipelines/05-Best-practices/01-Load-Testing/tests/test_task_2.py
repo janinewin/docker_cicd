43f632ee9b4249c525cc935b0ab3b19b0a2c0a1f
Yacine Alami <yacine.alami@gmail.com>
2022-06-20 22:19:31 +0200

    Manual challengify w2d5

---
+++ 02-Advanced-data-pipelines/05-Best-practices/01-Load-Testing/tests/test_task_2.py
@@ -0,0 +1,34 @@
+import pytest
+from yaml import load
+from yaml.loader import SafeLoader
+
+
+class TestTask2:
+    def test_docker_compose(self):
+        keys_to_check = [
+            "webapi",
+            "build",
+            "context",
+            "dockerfile",
+            "restart",
+            "ports",
+            "volumes",
+            "master",
+            "worker",
+            "env_file",
+        ]
+
+        def get_keys(d, keys=None):
+            keys = keys or []
+            if isinstance(d, dict):
+                keys += d.keys()
+                _ = [get_keys(x, keys) for x in d.values()]
+            elif isinstance(d, list):
+                _ = [get_keys(x, keys) for x in d]
+            return list(set(keys))
+
+        with open("docker-compose-task-2.yml") as f:
+            docker_compose_data = load(f, SafeLoader)
+
+            keys = get_keys(docker_compose_data)
+            assert all(item in keys for item in keys_to_check)
