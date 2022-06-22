aa053eb53fbc24176ac8bc17c45bdaab6fb61328
Selim <selimyoussry@gmail.com>
2022-06-22 16:30:54 +0200

    manual challengify

--- 01-Data-Storage-and-Batch-Pipelines/03-NoSQL/01-Track-Long/tests/test_sql.py
+++
@@ -1,5 +0,0 @@
-import lewagonde
-
-
-def test_genres_original_schema():
-    sql = "select count(*) from movies_metadata where genres ~ 'Drama';"
--- 01-Data-Storage-and-Batch-Pipelines/03-NoSQL/01-Track-Long/tests/test_transform.py
+++
@@ -1,3 +0,0 @@
-def test_transform():
-    # TODO @selim
-    pass
